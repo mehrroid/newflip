12263b3eabfac2addaa0da01652f21503d8b8326
n4554 <assemuni@gmail.com>
2017-03-26 01:28:34 +0430

    change

--- app/src/main/java/com/mehrit/newflip/MainActivity.java
+++ app/src/main/java/com/mehrit/newflip/MainActivity.java
@@ -2,15 +2,19 @@ package com.mehrit.newflip;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,18 +25,25 @@ public class MainActivity extends AppCompatActivity {
     private AnimatorSet mSetRightOut;
     private AnimatorSet mSetLeftIn;
     private boolean mIsBackVisible = false;
+    private TextViewListener textViewListener;
+    private FrameLayout mainFrameLayout;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FrameLayout mainFrameLayout = (FrameLayout) findViewById(R.id.frameLayoutActivity);
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        FrameLayout frameLayout = new FrameLayout(this);
+        mainFrameLayout = (FrameLayout) findViewById(R.id.frameLayoutActivity);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        frameLayout = new FrameLayout(this);
         frameLayout.setLayoutParams(params);
+        textViewListener =new TextViewListener();
+
+        /*
+
         //included for front of card
-        FrameLayout.LayoutParams params2 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        FrameLayout.LayoutParams params2 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         frameLayoutFront = new FrameLayout(this);
         frameLayoutFront.setLayoutParams(params2);
         TextView imgFront = new TextView(this);
@@ -40,26 +51,31 @@ public class MainActivity extends AppCompatActivity {
         imgFront.setTextSize(40);
         imgFront.setTextColor(Color.YELLOW);
         imgFront.setBackgroundResource(R.color.colorPrimaryDark);
+        imgFront.setOnClickListener(textViewListener);
 
 //        imgFront.setBackgroundResource(R.drawable.rectangle);
         frameLayoutFront.addView(imgFront);
         frameLayout.addView(frameLayoutFront);
         //
         //included2 for back of card
-        FrameLayout.LayoutParams params3 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    //    FrameLayout.LayoutParams params3 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         frameLayoutBack = new FrameLayout(this);
-        frameLayoutBack.setLayoutParams(params3);
+        frameLayoutBack.setLayoutParams(params2);
         TextView imgBack = new TextView(this);
         imgBack.setText("Back");
         imgBack.setTextSize(40);
         imgBack.setTextColor(Color.GREEN);
         imgBack.setBackgroundResource(R.color.colorAccent);
+        imgBack.setOnClickListener(textViewListener);
 //        imgBack.setBackgroundResource(R.drawable.rectangle);
         frameLayoutBack.addView(imgBack);
         frameLayout.addView(frameLayoutBack);
 //        View front = getLayoutInflater().inflate(R.layout.activity_front, null);
 //        frameLayout.addView(front);
         mainFrameLayout.addView(frameLayout);
+        */
+
+        createTextViews(1,2);
         loadAnimations();
         changeCameraDistance();
     }
@@ -76,20 +92,60 @@ public class MainActivity extends AppCompatActivity {
         mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.in_animation);
     }
 
-    public void flipCard(View view) {
-        if (!mIsBackVisible) {
-            mSetRightOut.setTarget(frameLayoutFront);
-            mSetLeftIn.setTarget(frameLayoutBack);
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mIsBackVisible = true;
+    private View createTextViews(int x, int y){
 
-        } else {
-            mSetRightOut.setTarget(frameLayoutBack);
-            mSetLeftIn.setTarget(frameLayoutFront);
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mIsBackVisible = false;
+
+//        Typeface fontawsome = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
+        FrameLayout.LayoutParams params2 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        frameLayoutFront = new FrameLayout(this);
+        frameLayoutFront.setLayoutParams(params2);
+        TextView imgFront = new TextView(this);
+        imgFront.setText("Front");
+        imgFront.setTextSize(40);
+        imgFront.setTextColor(Color.YELLOW);
+        imgFront.setBackgroundResource(R.color.colorPrimaryDark);
+        imgFront.setOnClickListener(textViewListener);
+        imgFront.setId(100*x+y);
+//        imgFront.setBackgroundResource(R.drawable.rectangle);
+        frameLayoutFront.addView(imgFront);
+        frameLayout.addView(frameLayoutFront);
+
+        frameLayoutBack = new FrameLayout(this);
+        frameLayoutBack.setLayoutParams(params2);
+        TextView imgBack = new TextView(this);
+        imgBack.setText("Back");
+        imgBack.setTextSize(40);
+        imgBack.setTextColor(Color.GREEN);
+        imgBack.setBackgroundResource(R.color.colorAccent);
+        imgBack.setOnClickListener(textViewListener);
+        imgFront.setId(1000*x+y);
+//        imgBack.setBackgroundResource(R.drawable.rectangle);
+        frameLayoutBack.addView(imgBack);
+        frameLayout.addView(frameLayoutBack);
+        mainFrameLayout.addView(frameLayout);
+
+        return frameLayout;
+    }
+
+
+    class TextViewListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Log.i("cards-onClick","in on TextViewListener");
+            if (!mIsBackVisible) {
+                mSetRightOut.setTarget(frameLayoutFront);
+                mSetLeftIn.setTarget(frameLayoutBack);
+                mSetRightOut.start();
+                mSetLeftIn.start();
+                mIsBackVisible = true;
+
+            } else {
+                mSetRightOut.setTarget(frameLayoutBack);
+                mSetLeftIn.setTarget(frameLayoutFront);
+                mSetRightOut.start();
+                mSetLeftIn.start();
+                mIsBackVisible = false;
+            }
         }
     }
 }
